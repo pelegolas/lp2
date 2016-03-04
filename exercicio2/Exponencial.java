994f1843ee66b2256a951a43d11a7c801db8be73
Pedro Barbosa <mustaph25@gmail.com>
2016-03-04 10:14:44 -0300

    Create Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Pelegolas O Elfo on 03/03/2016.
+ */
+public class Exponencial{
+    public static void main (String [] args){
+		System.out.println("#      Q       C");         
+		  for(int numero = 1; numero <=10; numero ++){
+			  System.out.println(numero + "     " + (numero*numero) + "      " + (numero*numero*numero));
+    	}
+	  }
+}
